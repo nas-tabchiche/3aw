55157b9119c88b3c6b1d8a62d3b2b826b21b69e4
Nassim Tabchiche <n.tabchiche2@gmail.com>
2023-11-28 14:19:24 +0100

    Define basic data model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Instance, Rule
 
-# Register your models here.
+
+class InstanceAdmin(admin.ModelAdmin):
+    list_display = ("host", "port")
+
+
+class RuleAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Instance, InstanceAdmin)
+admin.site.register(Rule, RuleAdmin)
